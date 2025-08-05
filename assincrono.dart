9be74230a4090922939bb3a47a11797bb1318b0d
Marcos Henrique Ribeiro de Santana. <161729448+MarcosHenriqueRS10@users.noreply.github.com>
2025-08-05 10:33:14 -0300

    Add files via upload

--- assincrono.dart
+++ assincrono.dart
@@ -1,6 +1,15 @@
-Future<void> realizarPix(String chave, double valor){
-  return Future.delayed(
-    Duration(seconds: 5),
-    () => print("Pix efetuado, comprovante: "),
- );// Future.delayed
+import 'package:assincrono/assincrono.dart' as assincrono;
+
+void main(List<String> arguments) {
+  assincrono.realizarPix("121323", 300)
+  .then(mensagemDeSucesso)
+  .catchError(mensagemDeErro);
+}
+
+void mensagemDeSucesso(void value){
+  print("Operação colcluida");
+}
+
+void mensagemDeErro(void value){
+  print("Ocorreu um erro");
 }
